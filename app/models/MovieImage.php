1aefd4e4f10d8ca0bcf9b25664bb6f099efe3935
benpoor <saske1984@126.com>
2014-05-07 15:01:47 +0800

    add

---
+++ app/models/MovieImage.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: benpoor2008
+ * Date: 14-5-7
+ * Time: 下午2:27
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MovieImage extends BaseModel{
+
+}
---
+++ app/models/Topic.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: benpoor2008
+ * Date: 14-5-7
+ * Time: 下午2:26
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Topic extends BaseModel{
+
+}
